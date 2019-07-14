1452eefcb71eb9607bd5e9889cd820e0226da98e
masiha68 <why_god88@yahoo.com>
2019-07-14 22:04:54 +0430

    first commit

---
+++ Market/Magento_Theme/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+    paths: {
+
+        typeit: 'Magento_Theme/js/typeit.min'
+        
+    },
+    shim: {
+
+        typeit: {
+            deps: ['jquery']
+        }
+    }
+};
---
+++ Market/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chiya
+ * Date: 5/17/19
+ * Time: 11:39 AM
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/WebGate/Market', __DIR__);
